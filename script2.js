5f639cdd4ed478756e7c11fa2e42bcc4bd9947bb
Alek148 <38920346+Alek148@users.noreply.github.com>
2021-01-29 17:00:24 +0100

    Added simple file loading

--- script2.js
+++ script2.js
@@ -3,3 +3,7 @@ const roomParam = urlParams.get('room');
 
 var room_select = document.getElementById("rooms");
 room_select.value = roomParam
+
+fetch('data/power.txt')
+  .then(response => response.text())
+  .then(text => console.log(text))
