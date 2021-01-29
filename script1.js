6255fdd53d87a4772cbf1098ab633f632f1bb80a
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2021-01-29 07:50:46 +0100

    Made the slider show its current value

--- script1.js
+++ script1.js
@@ -1,3 +1,11 @@
-const button = document.getElementById("hello_button")
+const button = document.getElementById("airTemperatureButton");
+const waterPumpValue = document.getElementById("waterPumpValue");
 
-button.addEventListener("click", function () { alert("Hello!") })
+function updateWaterPumpValue(value){
+    // \u00a0 is a non-breaking space
+    document.getElementById("waterPumpValue").textContent = "\u00a0\u00a0" + value;
+}
+
+button.addEventListener("click", function () {alert("Klikanie działa")})
+
+
