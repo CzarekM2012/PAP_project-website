136756ab510daa81669d1f2102f1e08372056663
Hubert Podlewski <h.podlewski@stud.elka.pw.edu.pl>
2021-01-29 19:36:42 +0100

    Added iron remover delay function and logging

--- main_window.js
+++ main_window.js
@@ -1,6 +1,9 @@
 //const button = document.getElementById("airTemperatureButton");
 //const waterPumpValue = document.getElementById("waterPumpValue");
 
+ironDelay = 0;
+
+
 function updateWaterPumpValue(value){
     // \u00a0 is a non-breaking space
     document.getElementById("waterPumpValue").textContent = "\u00a0\u00a0" + value;
@@ -12,6 +15,11 @@ function goToAirTemperatureDiagrams()
     document.location.href = "diagram.html";
     element = document.createElement("button");
     document.body.insertBefore(element, document.getElementById("rooms"));
+}
+
+function delayIronRemover(){
+    ironDelay += 15;
+    console.log("Changed iron remover delay to " + ironDelay);
 }
 
 function goToDiagram(room, parameter)
